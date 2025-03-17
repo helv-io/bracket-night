8f957a4e9cdd5a114a8ae9ecf577c1dd3d9ba598
Helvio Pedreschi <helvio88@gmail.com>
2025-03-17 19:43:52 -0400

    Add HTML Title and Meta Tags

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <link rel="icon" href="/favicon.ico" />
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <title>Bracket Night</title>
+        <meta name="description" content="Bracket Night - The ultimate bracket competition platform" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Bracket Night" />
+        <meta property="og:description" content="Bracket Night - The ultimate bracket competition platform" />
+        <meta property="og:image" content="/og-image.png" />
+        <meta property="og:url" content="https://bracket.helv.io" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
