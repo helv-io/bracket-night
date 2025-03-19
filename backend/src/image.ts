5cec3f56f1affc48e421b658cf334b7c7463511d
Helvio Pedreschi <helvio88@gmail.com>
2025-03-19 02:57:31 -0400

    TODO - committing for later work

--- backend/src/image.ts
+++ backend/src/image.ts
@@ -1,4 +1,5 @@
 import gis from 'async-g-i-s'
+import { Contestant } from 'types'
 
 // Create a new bracket with 16 contestants
 export const getImageURL = async (topic: string, choice: number): Promise<{ url: string, choice: number } | undefined> => {
@@ -29,5 +30,18 @@ export const getImageURL = async (topic: string, choice: number): Promise<{ url:
 // TODO: Add a function that fetches images from Google Image Search
 //       Convert them to 400x400px PNGs
 //       Save them to the filesystem so they can be used in the bracket
-//       Format: /app/data/images/<bracket_id>/<contestant_id>.png
+//       Format: /app/data/images/<bracket_id>_<contestant_id>.png
 //       <img> Fallback to Public URLs if the image is not found
+//       This must be done in the backend because the frontend cannot access the filesystem
+export const saveImages = async (url: string, contestant: Contestant): Promise<string> => {
+  // Fetch the image
+  const response = await fetch(url)
+  const blob = await response.blob()
+  const buffer = await blob.arrayBuffer()
+  console.log(buffer)
+  
+  // Save the image to the filesystem
+  const path = `/app/data/images/${contestant.bracket_id}_${contestant.id}.png`
+  
+  return path
+}
