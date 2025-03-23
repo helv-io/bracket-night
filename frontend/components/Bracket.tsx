02b31484de7eabc75403a386bcb6a7fe16049943
Helvio Pedreschi <helvio88@gmail.com>
2025-03-23 01:20:35 -0400

    Layout Fix

--- frontend/components/Bracket.tsx
+++ frontend/components/Bracket.tsx
@@ -167,7 +167,7 @@ const Bracket = ({ matchups, currentMatchupIndex }: BracketProps) => {
   }, [matchups, currentMatchupIndex])
 
   return (
-    <div ref={containerRef} style={{ position: 'relative', height: '100vh', width: '95%', margin: '0 auto' }}>
+    <div ref={containerRef} style={{ position: 'absolute', height: '100vh', width: '100%', margin: '0 auto', transform: 'translateX(-5px)' }}>
       <canvas ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }} />
       {/* Round 1 Left */}
       {matchups.slice(0, 4).map((matchup, index) => (
